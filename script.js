2f13477a37a3aa998f9fb3e19c4a7b95541cc03d
liuchaozhen <liuchaozhen@haier.com>
2026-07-17 10:46:52 +0800

    feat: draw initial circle

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const canvas = document.getElementById('animationCanvas');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+function drawCircle(x, y, radius) {
+    ctx.strokeStyle = 'rgba(0, 255, 255, 0.5)';
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.stroke();
+}
+// 初次渲染
+drawCircle(canvas.width / 2, canvas.height / 2, 100);
